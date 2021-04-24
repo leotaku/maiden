eddf06cf3fa49ef6fa043b6a2a039325d0bae4bc
Leo Gaskin <leo.gaskin@le0.gs>
2021-04-24 20:06:34 +0200

    Improve root command and add logger

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,31 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var (
-	noColorArg bool
+	noColorArg  bool
+	logLevelArg string
 )
 
 var rootCmd = &cobra.Command{
-	Use:                   "maiden [flags..] <subcommand>",
-	Short:                 "The Maiden task synchronization system",
-	Version:               "0.1",
+	Use:     "maiden [flags..] <subcommand>",
+	Short:   "The Maiden task synchronization system",
+	Version: "0.1",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return fmt.Errorf("Subcommand required")
+	},
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logrus.SetFormatter(&logrus.TextFormatter{DisableColors: noColorArg})
+		if lvl, err := logrus.ParseLevel(logLevelArg); err == nil {
+			logrus.SetLevel(lvl)
+		}
+	},
 	DisableFlagsInUseLine: true,
 }
 
@@ -24,10 +36,12 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.AddCommand(initCmd, serveCmd)
+	rootCmd.AddCommand(initCmd, syncCmd, serveCmd)
 	rootCmd.SetHelpFunc(help)
 	rootCmd.SetUsageFunc(usage)
 	_, noColor := os.LookupEnv("NO_COLOR")
-	rootCmd.Flags().BoolVarP(&noColorArg, "no-color", "n", noColor, "whether to disable colors in output")
+	rootCmd.PersistentFlags().BoolVarP(&noColorArg, "no-color", "n", noColor, "whether to disable colors in output")
+	rootCmd.PersistentFlags().StringVarP(&logLevelArg, "log", "l", "warning", "default logging level")
+	rootCmd.PersistentFlags().SortFlags = false
 	rootCmd.Flags().SortFlags = false
 }
