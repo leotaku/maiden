057378aa06beab127b5941d905f7c599552aa33e
Leo Gaskin <leo.gaskin@le0.gs>
2021-04-27 21:12:57 +0200

    Improve diary entry to VEvent conversion

--- diary/ical.go
+++ diary/ical.go
@@ -1,13 +1,53 @@
 package diary
 
 import (
+	"fmt"
 	"time"
 
 	ics "github.com/arran4/golang-ical"
 	"github.com/leotaku/maiden/timeutil"
 )
 
-func (e Entry) ToICALEvent(loc *time.Location) *ics.VEvent {
+func FromVEvent(ve *ics.VEvent) (*Entry, error) {
+	summary := ve.GetProperty(ics.ComponentPropertySummary)
+	if summary == nil || summary.Value == "" {
+		return nil, fmt.Errorf("missing event summary")
+	}
+
+	rrule := ve.GetProperty(ics.ComponentPropertyRrule)
+	if rrule != nil && rrule.Value != "" {
+		return nil, fmt.Errorf("recurring events are not currently supported")
+	}
+
+	var datetime timeutil.Datetime
+	start, err := getDatetimeProperty(ve, ics.ComponentPropertyDtStart)
+	if err != nil {
+		start, err = getDateProperty(ve, ics.ComponentPropertyDtStart)
+		if err != nil {
+			return nil, fmt.Errorf("start: %w", err)
+		} else {
+			datetime = timeutil.FromDate(*start)
+		}
+	} else {
+		datetime = timeutil.FromAbsolute(*start)
+	}
+
+	end, err := getDatetimeProperty(ve, ics.ComponentPropertyDtEnd)
+	if err != nil {
+		end, err = getDateProperty(ve, ics.ComponentPropertyDtEnd)
+		if err != nil {
+			end = start
+		}
+	}
+
+	return &Entry{
+		Datetime:    datetime,
+		Duration:    end.Sub(*start),
+		Description: summary.Value,
+	}, nil
+}
+
+func (e Entry) ToVEvent(loc *time.Location) *ics.VEvent {
 	ve := new(ics.VEvent)
 	ve.SetSummary(e.Description)
 	ve.SetCreatedTime(time.Now())
@@ -23,11 +63,17 @@ func (e Entry) ToICALEvent(loc *time.Location) *ics.VEvent {
 	start := e.Start(loc)
 	end := e.End(loc)
 	if e.Datetime.Time.Hour != nil && e.Datetime.Time.Minute != nil {
-		setLocalDateTime(ve, ics.ComponentPropertyDtStart, start)
-		setLocalDateTime(ve, ics.ComponentPropertyDtEnd, end)
+		setDatetimeProperty(ve, ics.ComponentPropertyDtStart, start)
+		setDatetimeProperty(ve, ics.ComponentPropertyDtEnd, end)
+	} else if e.Duration%(time.Hour*24) != 0 {
+		if e.Duration == 0 {
+			end = end.Add(time.Hour * 24)
+		}
+		setDatetimeProperty(ve, ics.ComponentPropertyDtStart, start)
+		setDatetimeProperty(ve, ics.ComponentPropertyDtEnd, end)
 	} else {
-		setLocalDate(ve, ics.ComponentPropertyDtStart, start)
-		setLocalDate(ve, ics.ComponentPropertyDtEnd, end)
+		setDateProperty(ve, ics.ComponentPropertyDtStart, start)
+		setDateProperty(ve, ics.ComponentPropertyDtEnd, end)
 	}
 
 	// Default reminder
@@ -45,12 +91,45 @@ func (e Entry) ToICALEvent(loc *time.Location) *ics.VEvent {
 	return ve
 }
 
-func setLocalDateTime(ve *ics.VEvent, prop ics.ComponentProperty, d time.Time) {
+func getDatetimeProperty(ve *ics.VEvent, prop ics.ComponentProperty) (*time.Time, error) {
+	p := ve.GetProperty(prop)
+	if p != nil && p.Value != "" {
+		loc := time.UTC
+		params := p.ICalParameters["TZID"]
+		if len(params) == 1 {
+			loc, _ = time.LoadLocation(params[0])
+		}
+
+		time, err := time.ParseInLocation(timeutil.DateTimeFormat, p.Value, loc)
+		if err != nil {
+			return nil, fmt.Errorf("parse: %w", err)
+		} else {
+			return &time, nil
+		}
+	} else {
+		return nil, fmt.Errorf("missing property: %v", prop)
+	}
+}
+
+func getDateProperty(ve *ics.VEvent, prop ics.ComponentProperty) (*time.Time, error) {
+	p := ve.GetProperty(prop)
+	if p != nil && p.Value != "" {
+		time, err := time.Parse(timeutil.DateFormat, p.Value)
+		if err != nil {
+			return nil, fmt.Errorf("parse: %w", err)
+		}
+		return &time, nil
+	} else {
+		return nil, fmt.Errorf("missing property: %v", prop)
+	}
+}
+
+func setDatetimeProperty(ve *ics.VEvent, prop ics.ComponentProperty, d time.Time) {
 	ve.SetProperty(prop, d.UTC().Format(timeutil.DateTimeFormat))
 }
 
-func setLocalDate(ve *ics.VEvent, prop ics.ComponentProperty, d time.Time) {
-	ve.SetProperty(prop, d.UTC().Format(timeutil.DateFormat), &ics.KeyValues{
+func setDateProperty(ve *ics.VEvent, prop ics.ComponentProperty, d time.Time) {
+	ve.SetProperty(prop, d.Format(timeutil.DateFormat), &ics.KeyValues{
 		Key:   "VALUE",
 		Value: []string{"DATE"},
 	})
